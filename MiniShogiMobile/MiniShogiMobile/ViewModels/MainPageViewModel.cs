8aad1a3357263ac3d2f3c383100ce9046571ed3a
hoge8086 <hoge8086@gmail.com>
2021-12-26 15:11:35 +0900

    Prismをインストール（動作確認含む）

--- MiniShogiMobile/MiniShogiMobile/ViewModels/MainPageViewModel.cs
+++ MiniShogiMobile/MiniShogiMobile/ViewModels/MainPageViewModel.cs
@@ -5,14 +5,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Reactive.Bindings;
 
 namespace MiniShogiMobile.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
+        public ReactiveProperty<string> Label { get; set; }
         public MainPageViewModel(INavigationService navigationService)
             : base(navigationService)
         {
+            Label = new ReactiveProperty<string>("あいうえお");
             Title = "Main Page";
         }
     }
