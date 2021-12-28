a8c69c060156adfc834d39b615e08db7c4211a0e
hoge8086 <hoge8086@gmail.com>
2021-12-29 00:42:27 +0900

    EnumListProviderを実装

---
+++ MiniShogiMobile/MiniShogiMobile/Utils/EnumListProvider.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms.Xaml;
+
+namespace MiniShogiMobile.Utils
+{
+    public class EnumItem<T>
+    {
+        public T Code { get; set; }
+        public string Name { get; set; }
+    }
+    public class EnumListProvider<T>: IMarkupExtension<IList<EnumItem<T>>>
+    {
+        private static string DisplayName(T value) {
+            var fileInfo = value.GetType().GetField(value.ToString());
+            var descriptionAttribute = (DescriptionAttribute)fileInfo
+                .GetCustomAttributes(typeof(DescriptionAttribute), false)
+                .FirstOrDefault();
+            return descriptionAttribute.Description;
+        }
+
+        public readonly static IList<EnumItem<T>> EnumItems = typeof(T).GetEnumValues()
+                                                        .Cast<T>()
+                                                        .Select(value => new EnumItem<T>{ Code = value, Name = DisplayName(value) })
+                                                        .ToList();
+        public IList<EnumItem<T>> ProvideValue(IServiceProvider serviceProvider)
+        {
+            return EnumItems;
+        }
+
+        object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider)
+        {
+            return (this as IMarkupExtension<IList<EnumItem<T>>>).ProvideValue(serviceProvider);
+        }
+    }
+}
--- MiniShogiMobile/MiniShogiMobile/ViewModels/StartGamePageViewModel.cs
+++ MiniShogiMobile/MiniShogiMobile/ViewModels/StartGamePageViewModel.cs
@@ -8,17 +8,43 @@ using MiniShogiMobile.Conditions;
 using System.Collections.ObjectModel;
 using Reactive.Bindings;
 using MiniShogiMobile.Views;
+using MiniShogiMobile.Utils;
+using System.ComponentModel;
 
 namespace MiniShogiMobile.ViewModels
 {
+    public class EnumPlayerTypeProvider : EnumListProvider<PlayerType> { }
+    public enum PlayerType
+    {
+        [Description("あなた")]
+        Human,
+        [Description("AI")]
+        AI,
+    };
+
+    public class EnumSelectFirstTurnPlayerProvider : EnumListProvider<SelectFirstTurnPlayer> { }
+    public enum SelectFirstTurnPlayer
+    {
+        [Description("ランダム")]
+        Random,
+        [Description("プレイヤー1")]
+        Player1,
+        [Description("プレイヤー2")]
+        Player2,
+    };
+
     public class StartGamePageViewModel : ViewModelBase
     {
         public ReactiveCommand PlayGameCommand { get; set; }
         public ObservableCollection<string> GameNameList { get; set; }
         public ReactiveProperty<string> GameName { get; set; }
+        public PlayperViewModel Player1 { get; set; }
+        public PlayperViewModel Player2 { get; set; }
         public StartGamePageViewModel(INavigationService navigationService) : base(navigationService)
         {
             GameName = new ReactiveProperty<string>();
+            Player1 = new PlayperViewModel(PlayerType.Human);
+            Player2 = new PlayperViewModel(PlayerType.AI, 5);
 
             PlayGameCommand = new ReactiveCommand();
             PlayGameCommand.Subscribe(() =>
@@ -31,6 +57,18 @@ namespace MiniShogiMobile.ViewModels
             GameNameList = new ObservableCollection<string>();
             foreach (var name in App.GameService.GameTemplateRepository.FindAllName())
                 GameNameList.Add(name);
+        }
+
+        public class PlayperViewModel : BindableBase
+        {
+            public ReactiveProperty<PlayerType> PlayerType { get; set; }
+            public ReactiveProperty<int> AIThinkDepth { get; set; }
+
+            public PlayperViewModel(PlayerType playerType, int depth = 0)
+            {
+                PlayerType = new ReactiveProperty<PlayerType>(playerType);
+                AIThinkDepth = new ReactiveProperty<int>(depth);
+            }
         }
     }
 }
