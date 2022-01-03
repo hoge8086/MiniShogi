1d02ccc6ad8b8961acc704cdf62b952da5e11551
hoge8086 <hoge8086@gmail.com>
2022-01-03 15:38:32 +0900

    プレイヤーアイコンを作成

---
+++ MiniShogiMobile/MiniShogiMobile/Controls/PlayerView.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MiniShogiMobile.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PlayerView : ContentView
+    {
+        public PlayerView()
+        {
+            InitializeComponent();
+        }
+
+        #region Text
+        public static readonly BindableProperty TextProperty = BindableProperty.Create(
+                                                                            nameof(Text),
+                                                                            typeof(string ),
+                                                                            typeof(PlayerView),
+                                                                            null);
+ 
+        /// <summary>
+        /// 表示名
+        /// </summary>
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+        #endregion
+
+        #region SubText
+        public static readonly BindableProperty SubTextProperty = BindableProperty.Create(
+                                                                            nameof(SubText),
+                                                                            typeof(string ),
+                                                                            typeof(PlayerView),
+                                                                            null);
+ 
+        /// <summary>
+        /// 表示名
+        /// </summary>
+        public string SubText
+        {
+            get { return (string)GetValue(SubTextProperty); }
+            set { SetValue(SubTextProperty, value); }
+        }
+        #endregion
+    }
+}
