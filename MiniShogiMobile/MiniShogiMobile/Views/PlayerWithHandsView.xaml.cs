12ded0571047274705c6303d8f5a897e3a1f4f29
hoge8086 <hoge8086@gmail.com>
2022-01-04 20:21:49 +0900

    PlayerWithHandsViewを作成

---
+++ MiniShogiMobile/MiniShogiMobile/Views/PlayerWithHandsView.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MiniShogiMobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PlayerWithHandsView : ContentView
+    {
+        public PlayerWithHandsView()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly BindableProperty ClickCommandProperty =
+            BindableProperty.Create(
+                nameof(ClickCommand), typeof(ICommand), typeof(PlayerWithHandsView),
+                defaultValue: new Command((obj) => {
+                    System.Diagnostics.Debug.WriteLine("CellView Tapped");
+                }));
+
+        public ICommand ClickCommand
+        {
+            get { return (ICommand)GetValue(ClickCommandProperty); }
+            set { SetValue(ClickCommandProperty, value); }
+        }
+
+        public static readonly BindableProperty IsRotatedProperty =
+            BindableProperty.Create(
+                nameof(IsRotated), typeof(bool), typeof(PlayerWithHandsView), false);
+
+        public bool IsRotated
+        {
+            get { return (bool)GetValue(IsRotatedProperty); }
+            set { SetValue(IsRotatedProperty, value); }
+        }
+
+
+        #region PlayerColor
+        public static readonly BindableProperty PlayerColorProperty = BindableProperty.Create(
+                                                                            nameof(PlayerColor),
+                                                                            typeof(Color),
+                                                                            typeof(PlayerWithHandsView),
+                                                                            null);
+ 
+        /// <summary>
+        /// 表示名
+        /// </summary>
+        public Color PlayerColor
+        {
+            get { return (Color)GetValue(PlayerColorProperty); }
+            set { SetValue(PlayerColorProperty, value); }
+        }
+        #endregion
+    }
+}
