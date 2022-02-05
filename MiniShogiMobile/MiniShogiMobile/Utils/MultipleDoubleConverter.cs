7b9f699114ff8299bab35ef23dd6dd5437444f5f
hoge8086 <hoge8086@gmail.com>
2022-02-05 13:08:21 +0900

    駒のサイズに応じて文字サイズが変わるように修正

---
+++ MiniShogiMobile/MiniShogiMobile/Utils/MultipleDoubleConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace MiniShogiMobile.Utils
+{
+    class MultipleDoubleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double multiplier;
+
+            if (!Double.TryParse(parameter as string, out multiplier))
+                multiplier = 1;
+
+            return multiplier * (double)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
