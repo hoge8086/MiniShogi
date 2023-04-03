d4a5a6f92320faa514942d644ad48e5bd36e9ca5
hoge8086 <hoge8086@gmail.com>
2023-04-03 23:59:31 +0900

    ホーム画面で戻るボタンが表示されてしまうため、ホーム画面のみナビゲーションバーを無効にする 戻るボタンの文字列を微修正

--- MiniShogiMobile/MiniShogiMobile.iOS/Renderers/CustomPageRenderer.cs
+++ MiniShogiMobile/MiniShogiMobile.iOS/Renderers/CustomPageRenderer.cs
@@ -11,6 +11,7 @@ using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
 
 //参考:<https://www.nuits.jp/entry/2016/11/20/185949>
+//問題点と処置：トップ画面も戻るボタンが表示されてしまう問題があるため、PLCでトップ画面のナビゲーションバーを無効にする
 [assembly: ExportRenderer(typeof(Page), typeof(CustomPageRenderer))]
 namespace MiniShogiMobile.iOS.Renderers
 {
@@ -23,7 +24,7 @@ namespace MiniShogiMobile.iOS.Renderers
             var page = Element as Page;
             var navigationPage = page.Parent as NavigationPage;
             var root = this.NavigationController.TopViewController;
-            root.NavigationItem.SetLeftBarButtonItem(new UIBarButtonItem($"< Back", UIBarButtonItemStyle.Plain, async (sender, args) =>
+            root.NavigationItem.SetLeftBarButtonItem(new UIBarButtonItem($"＜ 戻る", UIBarButtonItemStyle.Done, async (sender, args) =>
             {
                 //var navPage = page.Parent as NavigationPage;
                 var vm = page.BindingContext as NavigationViewModel;
