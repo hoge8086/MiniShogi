84c812c04f24590491f0a8efa06c200a2501ee7e
hoge8086 <hoge8086@gmail.com>
2023-04-23 23:39:02 +0900

    ライセンス情報を埋め込みリソースから取得して表示するように修正

--- MiniShogiMobile/MiniShogiMobile/ViewModels/LicensePageViewModel.cs
+++ MiniShogiMobile/MiniShogiMobile/ViewModels/LicensePageViewModel.cs
@@ -5,15 +5,37 @@ using Prism.NavigationEx;
 using Prism.Services;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 
 namespace MiniShogiMobile.ViewModels
 {
 	public class LicensePageViewModel : NavigationViewModel
 	{
+        public ObservableCollection<SoftwareLicense> Licenses { get; set; }
         public LicensePageViewModel(INavigationService navigationService, IPageDialogService pageDialogService) : base(navigationService, pageDialogService)
         {
+            Title = "ライセンス表記";
 
+            var licenses = Load(); 
+            Licenses = new ObservableCollection<SoftwareLicense>(licenses);
+        }
+        private List<SoftwareLicense> Load()
+        {
+            var assembly = typeof(LicensePageViewModel).GetTypeInfo().Assembly;
+            using (Stream stream = assembly.GetManifestResourceStream("MiniShogiMobile.Resources.Licenses.xml"))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                var serializer = new System.Xml.Serialization.XmlSerializer(typeof(List<SoftwareLicense>));
+                return  serializer.Deserialize(reader) as List<SoftwareLicense>;
+            }
         }
 	}
+    public class SoftwareLicense
+    {
+        public string SoftwareName { get; set; }
+        public string LicenseContext { get; set; }
+    }
 }
